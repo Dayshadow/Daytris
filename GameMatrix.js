a1db666e8814023bc8c5e3454437a69aa8fc2875
Dayshadow <Dayshadow101@gmail.com>
2021-11-27 11:01:44 -0500

    updates

--- GameMatrix.js
+++ GameMatrix.js
@@ -324,46 +324,17 @@ class Matrix {
         let boardWidth = this.columns * gridSize;
         let boardHeight = numDrawRows * gridSize;
 
-
-
-        ctx.fillStyle = rgb(
-            Math.floor(Math.sin(frame / 120 + 12.3) * 128) + 128,
-            Math.floor(Math.cos(frame / 100) * 128) + 128,
-            Math.floor(Math.sin(frame / 130 + 22.424) * 128) + 128
-        );
-        let fontSize = 20
-        ctx.font = fontSize + "px system-ui"
-        let titleText = "Welcome to my questionably coded tetris clone"
-        let titleWidth = titleText.length * fontSize * 0.465;
-        ctx.fillText(titleText, (w / 2) - titleWidth / 2, 50);
-
-        fontSize = 15
-        ctx.font = fontSize + "px system-ui";
-        ctx.fillText("Controls are:", 20, 100);
-        ctx.fillText("Left + Right arrow keys to move", 20, 100 + 2 * fontSize);
-        ctx.fillText("Z to spin counter clockwise", 20, 100 + 3 * fontSize);
-        ctx.fillText("Hold down arrow to fall faster", 20, 100 + 4 * fontSize);
-        ctx.fillText("Space to drop instantly", 20, 100 + 5 * fontSize);
-        ctx.fillText("C to hold a piece for later", 20, 100 + 6 * fontSize);
-        ctx.fillText("Minus key decreases the Delayed Auto Shift speed", 20, 100 + 8 * fontSize);
-        ctx.fillText("Plus key increases the Delayed Auto Shift speed", 20, 100 + 9 * fontSize);
-        ctx.fillText("Current frame count per DAS movement: " + this.DAS, 30, 100 + 10 * fontSize);
-        ctx.fillText("O decreases the Delayed Auto Shift delay", 20, 100 + 12 * fontSize);
-        ctx.fillText("P  increases the Delayed Auto Shift delay", 20, 100 + 13 * fontSize);
-        ctx.fillText("Amount of frames required to hold before auto shift: " + this.DASDelay, 30, 100 + 14 * fontSize);
-
-
         if (this.heldTetrimino) {
-            fontSize = 30
-            ctx.font = fontSize + "px system-ui"
-            let heldText = "Current Held Piece: ";
-            let heldTextWidth = heldText.length * fontSize * 0.465;
-            drawTetriminoOutline(this.heldTetrimino.data, (w / 2) + heldTextWidth / 2, h * 0.9, gridSize * 0.5, gridSize, ctx)
-            ctx.fillText(heldText, (w / 2) - heldTextWidth / 2, h * 0.92);
+            // fontSize = 30
+            // ctx.font = fontSize + "px system-ui"
+            // let heldText = "Current Held Piece: ";
+            // let heldTextWidth = heldText.length * fontSize * 0.465;
+            // drawTetriminoOutline(this.heldTetrimino.data, (w / 2) + heldTextWidth / 2, h * 0.9, gridSize * 0.5, gridSize, ctx)
+            // ctx.fillText(heldText, (w / 2) - heldTextWidth / 2, h * 0.92);
         }
 
 
-        fontSize = 40
+        let fontSize = 40
         ctx.font = fontSize + "px system-ui"
         let linesClearedText = "Lines Cleared: " + this.linesCleared;
         let linesClearedTextWidth = linesClearedText.length * fontSize * 0.465;
@@ -420,6 +391,7 @@ class Matrix {
             Math.floor(boardHeight + ctx.lineWidth * 2) - 3,
             [20, 20, 20, 20]
         )
+        //ctx.shadowBlur = 15
         ctx.fillStyle = rgb(255, 250, 191);
         drawShadedBorderPolygons(borderBox, ctx.lineWidth, 0.4, (frame / 200) % Math.PI * 2);
 
